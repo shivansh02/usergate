f44d432dc3285d28ddb04f8c51ca5f9f3bf3dcb1
shivansh02 <ss398@snu.edu.in>
2024-07-10 00:01:35 +0530

    signout working

--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -1,30 +1,32 @@
-import {auth} from '@/server/auth'
+import { auth } from '@/server/auth';
 import { UserButton } from './user-button';
 import { Button } from '@/components/ui/button';
-
+import Link from 'next/link';
 
 export default async function Nav() {
-    const session = await auth();
-    console.log(session);
+  const session = await auth();
+  console.log(session);
 
-    return(
-        <header>
-            <nav>
-                <ul>
-                    <li>logo</li>
-                    {!session ? (
-                        <li className="flex items-center justify-center">
-                        <Button asChild>
-                            Sign in
-                        </Button>
-                        </li>
-                    ) : (
-                        <li className="flex items-center justify-center">
-                        <UserButton expires={session?.expires} user={session?.user} />
-                        </li>
-                    )}
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="bg-gray-800 text-white">
+      <nav className="container mx-auto p-4 flex justify-between items-center">
+        <div className="text-xl font-bold">Logo</div>
+        <ul className="flex space-x-4">
+          {!session ? (
+            <li>
+              <Button asChild>
+                <Link className="flex gap-2 items-center" href="/api/auth/signin">
+                  <span>Login</span>
+                </Link>
+              </Button>
+            </li>
+          ) : (
+            <li>
+              <UserButton expires={session?.expires} user={session?.user} />
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
 }
--- components/navigation/user-button.tsx
+++ components/navigation/user-button.tsx
@@ -1,12 +1,76 @@
 "use client"
-import {Session} from "next-auth"
 
-export const UserButton = ({user}:Session) => {
+import { Session } from "next-auth"
+import { signOut } from "next-auth/react"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback } from "@/components/ui/avatar"
+import { GearIcon } from '@radix-ui/react-icons'
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+
+export const UserButton = ({ user }: Session) => {
+  const router = useRouter()
+
+  if (user)
     return (
-        <div>
-            <h1>
-                {user?.email}
-            </h1>
-        </div>
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger>
+          <Avatar className="w-7 h-7">
+            {user.image && (
+              <Image src={user.image} alt={user.name!} fill={true} />
+            )}
+            {!user.image && (
+              <AvatarFallback className="bg-primary/25">
+                <div className="font-bold">
+                  {user.name?.charAt(0).toUpperCase()}
+                </div>
+              </AvatarFallback>
+            )}
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-64 p-6" align="end">
+          <div className="mb-4 p-4 flex flex-col gap-1 items-center rounded-lg bg-primary/10">
+            {user.image && (
+              <Image
+                src={user.image}
+                alt={user.name!}
+                className="rounded-full"
+                width={36}
+                height={36}
+              />
+            )}
+            <p className="font-bold text-xs">{user.name}</p>
+            <span className="text-xs font-medium text-secondary-foreground">
+              {user.email}
+            </span>
+          </div>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            onClick={() => router.push("/dashboard/settings")}
+            className="group py-2 font-medium cursor-pointer ease-in-out"
+          >
+            <GearIcon
+              className="mr-3 group-hover:rotate-180 transition-all duration-300 ease-in-out"
+            />
+            Settings
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => signOut()}
+            className="py-2 group focus:bg-destructive/30 font-medium cursor-pointer"
+          >
+            {/* <LogOut
+              size={14}
+              className="mr-3 group-hover:scale-75 transition-all duration-300 ease-in-out"
+            /> */}
+            Sign out
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     )
 }
